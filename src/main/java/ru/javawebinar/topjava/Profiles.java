11c41eb9d371eed2b9d27b1aec9de393da9254b1
MrPetrovs <smokik1@gmail.com>
2022-02-15 13:20:29 +0200

    5 7 spring data jpa

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -15,4 +15,14 @@ public class Profiles {
             HSQL_DB = "hsqldb";
 
     public static final String ACTIVE_DB = POSTGRES_DB;
+    //  Get DB profile depending of DB driver in classpath
+    public static String getActiveDbProfile() {
+        if (ClassUtils.isPresent("org.postgresql.Driver", null)) {
+            return POSTGRES_DB;
+        } else if (ClassUtils.isPresent("org.hsqldb.jdbcDriver", null)) {
+            return HSQL_DB;
+        } else {
+            throw new IllegalStateException("Could not find DB driver");
+        }
+    }
 }
